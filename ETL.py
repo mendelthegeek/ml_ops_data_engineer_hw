440325cc84cf00e95a06c487be2de0dbf80016ba
mendelkeller <mendelthehumble@gmail.com>
2024-06-18 11:19:50 -0700

    added comments, only derive features if exist, separated data by uuid, two options for output

--- ETL.py
+++ ETL.py
@@ -5,74 +5,86 @@ import numpy as np
 import pandas as pd
 
 
+# pivots table into time series measuring features
 def widen(df):
     wide = df.pivot_table(index='time', columns=["field", "robot_id"], values="value")
-    wide.columns = wide.columns.map(lambda x: x[0] + "_" + str(x[1]))
-    wide.index = pd.to_datetime(wide.index, format="mixed")
+    wide.columns = wide.columns.map(lambda x: x[0] + "_" + str(x[1]))  # collapse multiindex
+    wide.index = pd.to_datetime(wide.index, format="mixed")  # store as time series
 
     return wide
 
 
+# derives velocity and acceleration from position
 def robot_motion(wide_df):
+    # for calculation of motion metrics, we linearly interpolate
     interpolate_fill = wide_df.interpolate(method="time")
-
+    # calculates velocity
     position_columns = [col_name for col_name in interpolate_fill.columns if "f" not in col_name]
-    positions = interpolate_fill[position_columns]
+    positions = interpolate_fill[position_columns]  # extract position columns
     velocity = positions.apply(
         lambda p:
         p / positions.index.diff().total_seconds() * 1000,
         axis=0
-    )
-    velocity.columns = ["v" + col_name for col_name in velocity.columns]
-
+    )  # velocity as position-delta/time, in mm/ms
+    velocity.columns = ["v" + col_name for col_name in velocity.columns]  # vx_i, vy_i, vz_i
+    # calculates acceleration, similar to above for velocity
     acceleration = velocity.apply(
         lambda v:
         v / velocity.index.diff().total_seconds() * 1000,
         axis=0
-    )
-    acceleration.columns = ["a" + col_name[1:] for col_name in acceleration.columns]
+    )  # acceleration as velocity-delta/time, in mm/ms^2
+    acceleration.columns = ["a" + col_name[1:] for col_name in acceleration.columns]  # ax_i, ay_i, az_i
     return velocity.join(acceleration)
 
 
+# gets absolute metrics from vector metrics
 def calculate_vectors(wide, motion, robot_ids):
-    vector_lengths = pd.DataFrame()
+    vector_lengths = pd.DataFrame()  # empty frame to store outputs
 
+    # helper function for vector length, to avoid repeating code
     def vector_length(cols):
-        return np.sqrt(
-            cols.apply(lambda x: x ** 2)
+        return np.sqrt(  # euclidean vector size
+            cols.apply(lambda x: x ** 2)  # run on all passed columns
             .sum(axis=1)
         )
-
+    # runs on each provided robot id
     for r_id in robot_ids:
-        v_columns_w_id = motion.filter(regex=f"v[xyz]_{str(r_id)}")
-        a_columns_w_id = motion.filter(regex=f"a[xyz]_{str(r_id)}")
-        f_columns_w_id = wide.filter(regex=f"f[xyz]_{str(r_id)}")
-        vector_lengths[f"v_{str(r_id)}"] = vector_length(v_columns_w_id)
-        vector_lengths[f"a_{str(r_id)}"] = vector_length(a_columns_w_id)
-        vector_lengths[f"f_{str(r_id)}"] = vector_length(f_columns_w_id)
+        v_columns_w_id = motion.filter(regex=f"v[xyz]_{str(r_id)}")  # extract velocity columns
+        a_columns_w_id = motion.filter(regex=f"a[xyz]_{str(r_id)}")  # extract acceleration columns
+        f_columns_w_id = wide.filter(regex=f"f[xyz]_{str(r_id)}")  # extract force columns
+        # check for empty to avoid writing unnecessary columns
+        if not v_columns_w_id.empty:
+            vector_lengths[f"v_{str(r_id)}"] = vector_length(v_columns_w_id)  # v_i
+        if not a_columns_w_id.empty:
+            vector_lengths[f"a_{str(r_id)}"] = vector_length(a_columns_w_id)  # a_i
+        if not f_columns_w_id.empty:
+            vector_lengths[f"f_{str(r_id)}"] = vector_length(f_columns_w_id)  # f_i
 
     return vector_lengths
 
 
+# runs main functions sequentially to produce supertable
 def calculate_features(df):
-    wide = widen(df)
-
-    motion = robot_motion(wide)
-
-    vector_lengths = calculate_vectors(wide, motion, df["robot_id"].unique().tolist())
-
+    wide = widen(df)  # reshape data
+    motion = robot_motion(wide)  # add velocity, acceleration
+    vector_lengths = calculate_vectors(wide, motion, df["robot_id"].unique().tolist())  # get absolute sizes
+    # produce a single supertable
     calculated = wide.join(motion).join(vector_lengths)
-    return calculated.bfill()
+    return calculated.bfill()  # fills in remaining NaNs
 
 
-def distance_traveled(wide):
+# calculates total distance traveled by each robot
+def distance_travelled(wide):
+    distances = []
+    # for each robot separately
+    for i in range(2):  # i = 0, 1 for robot i+1 = 1, 2
+        # extract all position columns e.g. (x_1, y_1 , z_1) for robot i+1 = 1, 2
+        # this doesn't assume any such columns, but extracts any which we do have
+        position_columns = list(filter(lambda s: re.match(f"[xyz]_{str(i+1)}", s), wide.columns))
+        # produces a list of pairs of successive vectors
+        # Nan entries will introduce errors, and add nothing useful, so we drop them
+        calculate = pairwise(wide[position_columns].dropna().values)
+        # sum the distance traveled between all pairs of successive points
+        distances.append(sum([np.linalg.norm(b - a) for a, b in calculate]))
 
-    position_columns_1 = list(filter(lambda s: re.match("[xyz]_1", s), wide.columns))
-    calculate = pairwise(wide[position_columns_1].dropna().values)
-    distance_1 = sum([np.linalg.norm(b - a) for a, b in calculate])
-
-    position_columns_2 = list(filter(lambda s: re.match("[xyz]_2", s), wide.columns))
-    calculate = pairwise(wide[position_columns_2].dropna().values)
-    distance_2 = sum([np.linalg.norm(b - a) for a, b in calculate])
-
-    return distance_1, distance_2
+    return distances  # returns size 2 list of distance travelled for each robot
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,18 +1,15 @@
+import os
+import sys
+
 import pandas as pd
 import ETL
 
+mode = (sys.argv[1])
+
 pd.set_option('display.max_columns', None)
-
+# reads in data
 df = pd.read_parquet("./data/sample.parquet")
-
-wide = ETL.widen(df)
-motion = ETL.robot_motion(wide)
-vector_lengths = ETL.calculate_vectors(wide, motion, df["robot_id"].unique().tolist())
-
-wide.to_csv("./output/wide.csv")
-motion.to_csv("./output/motion.csv")
-vector_lengths.to_csv("./output/vector_lengths.csv")
-
+# setup to store runtime stats
 runtime_stats = pd.DataFrame(columns=[
     "uuid",
     "start",
@@ -25,11 +22,32 @@ runtime_stats = pd.DataFrame(columns=[
 runtime_stats.set_index("uuid", inplace=True)
 runs_by_uuid = dict(tuple(df.groupby('run_uuid')))
 
+# run analysis on each uuid run
 for uuid, run in runs_by_uuid.items():
-    formatted = ETL.widen(run)
-    start = formatted.index.min()
-    end = formatted.index.max()
-    distance_1, distance_2 = ETL.distance_traveled(formatted)
+    # ensure output dir exists
+    outdir = f"./output/{uuid}"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    if mode == "separate":
+        # pivot, interpolate, and derive feature
+        wide = ETL.widen(run)
+        motion = ETL.robot_motion(wide)
+        vector_lengths = ETL.calculate_vectors(wide, motion, df["robot_id"].unique().tolist())
+        # write results into separate csvs
+        wide.to_csv(outdir + "/wide.csv")
+        motion.to_csv(outdir + "/motion.csv")
+        vector_lengths.to_csv(outdir + "/vector_lengths.csv")
+
+    elif mode == "full":
+        ETL.calculate_features(run).to_csv(outdir+"/full.csv")
+
+    wide = ETL.widen(run)
+    # derive runtime stats
+    start = wide.index.min()
+    end = wide.index.max()
+    distance_1, distance_2 = ETL.distance_travelled(wide)
+    # append runtime stats
     runtime_stats.loc[uuid] = [
         start,
         end,
@@ -38,5 +56,5 @@ for uuid, run in runs_by_uuid.items():
         distance_2,
         distance_1 + distance_2
     ]
-
+# write runtime stats
 runtime_stats.to_csv("./output/runtime_stats.csv")
